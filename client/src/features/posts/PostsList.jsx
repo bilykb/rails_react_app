049184868307544638084186ccedcd7b2f2e668d
bilykb <brettbilyk@gmail.com>
2024-05-21 14:53:49 -0600

    init vite, add first PostsLists component

---
+++ client/src/features/posts/PostsList.jsx
@@ -0,0 +1,13 @@
+ // API_URL comes from the .env.development file
+ import { Fragment } from "react"
+ 
+ export default function PostsList() {
+    
+    return (
+            <>
+                <div></div>
+            </>
+
+        )
+    
+}
